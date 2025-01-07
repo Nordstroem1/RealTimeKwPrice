ed78b72b6429869f9ada08e5d8d18deca74259ba
Nordstroem1 <johannord27@hotmail.com>
2025-01-07 10:56:35 +0100

    waiting for the dbcontext and the database then we can uncomment the code and use the crudoperations to the database using this.

---
+++ RealTimeKwPrice/Infrastructure/Data/GenericRepository.cs
@@ -0,0 +1,48 @@
+﻿//using Domain.Interfaces;
+//using Microsoft.EntityFrameworkCore;
+//using System.Linq.Expressions;
+
+//namespace Infrastructure.Data
+//{
+//    public class GenericRepository<T> : IGenericRepository<T> where T : class
+//    {
+
+//        private readonly ApplicationDbContext _context;
+//        private readonly DbSet<T> _dbSet;
+//        public GenericRepository(mySqlDb mySqlDb)
+//        {
+//            _mySqlDb = mySqlDb;
+//            _dbSet = _mySqlDb.Set<T>();
+//        }
+//        public async Task<T>? GetByIdAsync(Guid id)
+//        {
+//            //return await _dbSet.FindAsync(id);
+//        }
+//        public async Task<IEnumerable<T>> GetAllAsync()
+//        {
+//            return await _dbSet.ToListAsync();
+//        }
+//        public async Task<IEnumerable<T>> Find(Expression<Func<T, bool>> expression)
+//        {
+//            return await _dbSet.Where(expression).ToListAsync();
+//        }
+//        public async Task<T> AddAsync(T entity)
+//        {
+//            await _dbSet.AddAsync(entity);
+//            await _mySqlDb.SaveChangesAsync();
+//            return entity;
+//        }
+//        public async Task<T> DeleteAsync(T entity)
+//        {
+//            _dbSet.Remove(entity);
+//            await _mySqlDb.SaveChangesAsync();
+//            return entity;
+//        }
+//        public async Task<T> UpdateAsync(T entity)
+//        {
+//            _dbSet.Update(entity);
+//            await _mySqlDb.SaveChangesAsync();
+//            return entity;
+//        }
+//    }
+//}
