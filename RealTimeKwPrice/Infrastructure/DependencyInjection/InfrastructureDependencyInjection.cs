81b180d4506cb1fee61d1818539d8bedb091abf9
Nordstroem1 <johannord27@hotmail.com>
2025-01-07 17:19:10 +0100

    Added roles(admin,user and superadmin) added a tokenhelper to be used in the Login. RoleInitializer is there to make sure the database has all the roles and Initializes them all, also finetuned the UserClass and added the DependencyInjection folder in the Infrastructur layer.

---
+++ RealTimeKwPrice/Infrastructure/DependencyInjection/InfrastructureDependencyInjection.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace Infrastructure.DependencyInjection
+{
+    public static class InfrastructureDependencyInjection
+    {
+        public static IServiceCollection AddInfrastructureLayer(this IServiceCollection services, IConfiguration configuration)
+        {
+            var jwtSettings = configuration.GetSection("JwtSettings");
+            var secretKey = jwtSettings["secret"];
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = jwtSettings["Issuer"],
+                    ValidAudience = jwtSettings["Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey))
+                };
+            });
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("Superadmin", policy =>
+                {
+                    policy.AuthenticationSchemes.Add(JwtBearerDefaults.AuthenticationScheme);
+                    policy.RequireAuthenticatedUser();
+                    policy.RequireRole("Superadmin");
+                });
+                options.AddPolicy("Admin", policy =>
+                {
+                    policy.AuthenticationSchemes.Add(JwtBearerDefaults.AuthenticationScheme);
+                    policy.RequireAuthenticatedUser();
+                });
+                options.AddPolicy("User", policy =>
+                {
+                    policy.AuthenticationSchemes.Add(JwtBearerDefaults.AuthenticationScheme);
+                    policy.RequireAuthenticatedUser();
+                });
+            });
+
+            return services;
+        }
+    }
+}
---
+++ RealTimeKwPrice/Infrastructure/Initializers/RoleInitializer.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace Infrastructure.Initializer
+{
+    public class RoleInitializer
+    {
+        public static async Task InitializeAsync(RoleManager<IdentityRole<Guid>> roleManager)
+        {
+            foreach (var roleName in Enum.GetNames(typeof(RoleEnums.Roles)))
+            {
+                if (!await roleManager.RoleExistsAsync(roleName))
+                {
+                    await roleManager.CreateAsync(new IdentityRole<Guid> { Name = roleName });
+                }
+            }
+        }
+    }
+}
